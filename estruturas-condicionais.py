e2f0f65224604a90eb861969e19e9fe90696cf75
Isaac Dias <isaacardias@gmail.com>
2018-10-02 21:06:32 -0300

    correção do formato para melhor visualização

---
+++ estruturas-condicionais.py
@@ -0,0 +1,28 @@
+# Estruturas condicionais
+
+#
+# if's simples
+#
+
+if expressão:
+    comando
+
+#
+#if's comuns...
+#
+
+if expressão:
+    comando
+else:
+    comando
+
+#
+# Laço 'else if'
+#
+
+if expressão:
+    comando
+elif:
+    comando
+else:
+    comando
